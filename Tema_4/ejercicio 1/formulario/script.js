6ee20e4e96e56974144bcc7f22b361cf341c2c64
Ivan <icarreras19961@iesjoanramis.org>
2023-11-23 20:13:47 +0100

    23/11 creada la base de datos en js

--- Tema_4/ejercicio 1/formulario/script.js
+++ Tema_4/ejercicio 1/formulario/script.js
@@ -4,23 +4,58 @@ var indexedDB =
   window.webkitIndexedDB ||
   window.msIndexedDB ||
   window.shimIndexedDB;
-var data = "userDB";
-var db;
-var opened = false;
-const EDIT_USER = "Edit user";
-const NEW_USER = "New user";
-const ADD_USER = "Add user";
+let db;
 
-/**
- * Create db
- */
-function openCreateDb(onDbCompleted) {
-  if (opened) {
-    db.close();
-    opened = false;
-  }
-  //We could changing version
-  var req = indexedDB.open(database, DB_VERSION);
+//crea la base de datos o la abre si esta creada.
+function iniciarDB() {
+  let btnGuardar = document.querySelector("submit");
+  btnGuardar.addEventListener("click", almacenarUser);
 
-  //
+  let solicitud = indexedDB.open("Datos-de-formulario");
+
+  solicitud.addEventListener("error", mostrarError);
+  solicitud.addEventListener("success", comenzar);
+  solicitud.addEventListener("upgradeneeded", crearAlmacen);
 }
+
+//En caso de que falle la peticio
+function mostrarError(error) {
+  console.log("Hay un error en: " + error.code + " / " + error.message);
+}
+//en caso de que se arranque correctamente
+function comenzar(acceso) {
+  db = acceso.target.result;
+  console.log(db);
+}
+
+//Esto crea la estructura de la base de datos
+function crearAlmacen(evento) {
+  console.log(evento.target.result);
+  let database = evento.target.result;
+  //La tabla
+  let almacen = database.createObjectStore("User", { keyPath: "nombre" });
+  //las fields
+  // almacen.createIndex("Nombre", "Nombre", { unique: false });
+  almacen.createIndex("Apellido", "Apellido", { unique: false });
+  almacen.createIndex("Email", "Email", { unique: false });
+  almacen.createIndex("Telefono", "Telefono", { unique: false });
+  almacen.createIndex("Avatar", "Avatar", { unique: false });
+}
+
+function almacenarUser() {
+  let nombre = document.querySelector("#nombre").value;
+  let apellido = document.querySelector("#apellido").value;
+  let email = document.querySelector("#email").value;
+  let telefono = document.querySelector("#telefono").value;
+
+  let transaccion = db.transaction(["User"], "readwrite");
+  let almacen = transaccion.objectStore("User");
+
+  almacen.add({
+    Nombre: nombre,
+    Apellido: apellido,
+    Email: email,
+    Telefono: telefono,
+  });
+}
+window.addEventListener("load", iniciarDB());
