145e9ae069e0b98a62d9e86d576643dfd2f9d5a1
Ivan <icarreras19961@iesjoanramis.org>
2023-12-13 15:24:33 +0100

    13/12 ejercicio 1 acabado

--- Tema_5/ejercicio1/script.js
+++ Tema_5/ejercicio1/script.js
@@ -15,19 +15,39 @@ var sales = [
     units: 300,
   },
 ];
+console.log(sales[0].units);
 context.beginPath();
 
 //Las palabras
 context.font = "bold 30px serif";
-context.fillText("Units",50,200);
-context.fillText("Basketball",160,440);
-context.fillText("Baseball",320,440);
-context.fillText("Football",480,440);
-context.fillText("Products",  300,500);
+context.fillText("Units", 50, 200);
+context.fillText(sales[0].product, 160, 440);
+context.fillText(sales[1].product, 320, 440);
+context.fillText(sales[2].product, 480, 440);
+context.fillText("Products", 300, 500);
 
 //Las flechitas
 context.moveTo(150, 0);
-context.lineTo(150,400);
-context.lineTo(700,400);
-
+context.lineTo(150, 400);
+context.lineTo(700, 400);
 context.stroke();
+
+//Los valores
+
+const grd = context.createLinearGradient(200, 400, 200, 80);
+grd.addColorStop(0, "orange");
+grd.addColorStop(1, "white");
+context.fillStyle = grd;
+context.fillRect(200, 400, 50, -(sales[0].units));
+
+const grd2 = context.createLinearGradient(350, 400, 600, 0);
+grd2.addColorStop(0, "blue");
+grd2.addColorStop(1, "white");
+context.fillStyle = grd2;
+context.fillRect(350, 400, 50, -(sales[1].units));
+
+const grd3 = context.createLinearGradient(500, 400, 600, 50);
+grd3.addColorStop(0, "red");
+grd3.addColorStop(1, "white");
+context.fillStyle = grd3;
+context.fillRect(500, 400, 50, -(sales[2].units));
