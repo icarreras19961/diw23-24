0c1ec44431cd4227d35190145540072252ebb2bd
Ivan <icarreras19961@iesjoanramis.org>
2024-01-11 21:16:02 +0100

    11/01 array hecho

--- Tema_6/Jquery/ejercicios/ejercicio2/memory.js
+++ Tema_6/Jquery/ejercicios/ejercicio2/memory.js
@@ -2,9 +2,33 @@ $(document).ready(function () {
   console.log("Loaded");
 
   let ncards = Math.floor((Math.random() * 20) / 2) * 2 + 2;
+
+  let arrayJuego = [];
+
   console.log(ncards);
+  rellenador(arrayJuego, ncards);
+  console.log(arrayJuego);
   for (let i = 1; i <= ncards; i++) {
-    console.log("hola");
-    $("#bodyMem").append('<div id="rana"><img src="imagenes/Original.png" alt=""></div>')
+    $("#bodyMem").append(
+      `<div class="rana"><img src="imagenes/Original.png"><span class="number">${
+        arrayJuego[i - 1]
+      }</span></div>`
+    );
   }
+
+  function rellenador(arrayJuego, ncards) {
+    console.log(arrayJuego, ncards);
+    for (let i = 1; i <= ncards / 2; i++) {
+      arrayJuego[i] = i;
+    }
+    for (let i = 1; i <= ncards / 2; i++) {
+      arrayJuego.push(i);
+    }
+    arrayJuego = arrayJuego.sort(() => {
+      return Math.random() * -0.5;
+    });
+  }
+  $(".rana").on("click", (e)=> {
+    $(".number").css("display", "block");
+  });
 });
