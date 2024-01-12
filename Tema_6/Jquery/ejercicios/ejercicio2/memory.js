b062d6b936c699258cfa7d0be70b8c3819c7ddc8
Ivan <icarreras19961@iesjoanramis.org>
2024-01-12 16:20:10 +0100

    19/01 haciendo el comprobador de numeros

--- Tema_6/Jquery/ejercicios/ejercicio2/memory.js
+++ Tema_6/Jquery/ejercicios/ejercicio2/memory.js
@@ -1,21 +1,26 @@
 $(document).ready(function () {
   console.log("Loaded");
 
-  let ncards = Math.floor((Math.random() * 20) / 2) * 2 + 2;
-
+  let ncards = Math.floor(Math.random() * (24 - 12) + 12);
   let arrayJuego = [];
+  let tries = [];
+  let ntries = 0;
+  if (ncards % 2 != 0) {
+    ncards++;
+  }
 
-  console.log(ncards);
   rellenador(arrayJuego, ncards);
   console.log(arrayJuego);
+  //Fill the desk with the frogs
   for (let i = 1; i <= ncards; i++) {
     $("#bodyMem").append(
-      `<div class="rana"><img src="imagenes/Original.png"><span class="number">${
+      `<div class="frog"><img src="imagenes/Original.png" class="img_frog"><span class="number">${
         arrayJuego[i - 1]
       }</span></div>`
     );
   }
 
+  //Fill the array with random numbers that are extract on the half of the ncard value
   function rellenador(arrayJuego, ncards) {
     console.log(arrayJuego, ncards);
     for (let i = 1; i <= ncards / 2; i++) {
@@ -25,10 +30,24 @@ $(document).ready(function () {
       arrayJuego.push(i);
     }
     arrayJuego = arrayJuego.sort(() => {
-      return Math.random() * -0.5;
+      return Math.random() - 0.5;
     });
   }
-  $(".rana").on("click", (e)=> {
-    $(".number").css("display", "block");
+
+  //The listeners that you click and do thinks
+  $(".frog").on("click",  function(e) {
+    $(this).addClass("check");
+    // console.log($(this).text());
+    tries[ntries] = $(this).text();
+    ntries++;
+    analizator(tries);
   });
+
+  function analizator(tries) {
+    console.log(tries.length);
+    if (tries.length == 2) {
+      tries.splice();
+    }
+    console.log(tries);
+  }
 });
