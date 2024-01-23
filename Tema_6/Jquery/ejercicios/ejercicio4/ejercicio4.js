2a242290a861ddfdd03584bfa114cc09b18734fc
Ivan <icarreras19961@iesjoanramis.org>
2024-01-24 01:38:35 +0700

    24/01 parte de arrastrar casi acabado

--- Tema_6/Jquery/ejercicios/ejercicio4/ejercicio4.js
+++ Tema_6/Jquery/ejercicios/ejercicio4/ejercicio4.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
+  let colorClass;
+  let ntooDo = 0;
+  let ndoing = 0;
+  let ndonete = 0;
   $("#genereitor").on("click", () => {
     let colorRand = Math.floor(Math.random() * (4 - 1) + 1);
-    let colorClass;
-    let ntooDo = 0;
-    let ndoing = 0;
-    let ndonete = 0;
 
     if (colorRand == 1) {
       colorClass = "red";
@@ -14,27 +14,53 @@ $(document).ready(function () {
       colorClass = "green";
     }
     $("#genereitor").after(`<div class='posit ${colorClass}'></div>`);
+    if (
+      $(".posit").data("droped") == true ||
+      !$(".posit").data("droped") == undefined
+    ) {
+      $(".posit").data("droped", true);
+    } else {
+      $(".posit").data("droped", false);
+    }
+    console.log($(".posit").data("droped"));
     $(".posit").draggable();
-    $("#tooDo").droppable({
-      accept: ".red",
-      drop: function () {
+  });
+  $("#tooDo").droppable({
+    accept: ".red",
+    drop: function (event, e) {
+      console.log($(e.draggable).data());
+      if ($(e.draggable).data("droped") == false) {
+        $(e.draggable).data("droped", true);
         ntooDo++;
-        $(this).find("span").text(ntooDo);
-      },
-    });
-    $("#doing").droppable({
-      accept: ".blue",
-      drop: function () {
+      }
+      console.log(ntooDo);
+      $(this).find("span").text(ntooDo);
+    },
+  });
+  $("#doing").droppable({
+    accept: ".blue",
+    drop: function (event, e) {
+      if ($(e.draggable).data("droped") == false) {
+        $(e.draggable).data("droped", true);
         ndoing++;
-        $(this).find("span").text(ndoing);
-      },
-    });
-    $("#donete").droppable({
-      accept: ".green",
-      drop: function () {
+      }
+      $(this).find("span").text(ndoing);
+    },
+  });
+  $("#donete").droppable({
+    accept: ".green",
+    drop: function (event, e) {
+      if ($(e.draggable).data("droped") == false) {
+        $(e.draggable).data("droped", true);
         ndonete++;
-        $(this).find("span").text(ndonete);
-      },
-    });
+      }
+      $(this).find("span").text(ndonete);
+    },
+  });
+  $("main").droppable({
+    drop: function (event, e) {
+      if ($(e.draggable).data("droped") == true) {
+      }
+    },
   });
 });
