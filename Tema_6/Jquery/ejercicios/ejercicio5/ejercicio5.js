f58c016839fb15590b64a447c42a4686e12a7564
Ivan <icarreras19961@iesjoanramis.org>
2024-01-31 17:31:30 +0100

    31/01 sponguifai

---
+++ Tema_6/Jquery/ejercicios/ejercicio5/ejercicio5.js
@@ -0,0 +1,62 @@
+var client_id = "cd351f180b6542998bc2c6e359c76b0a";
+var client_secret = "b7b1e82f11cd42608074f003fce63b36";
+var access_token = "11dFghVXANMlKmJXsNCbNl";
+
+//We create the Spotify class with the API to make the call to
+function Spotify() {
+  this.apiUrl = "https://api.spotify.com/";
+}
+
+//Search for information on an artist, adding the possibility of obtaining their albums.
+Spotify.prototype.getArtist = function (artist) {
+  $.ajax({
+    type: "GET",
+    url: this.apiUrl + "v1/search?type=artist&q=" + artist,
+    headers: {
+      Authorization: "Bearer " + access_token,
+    },
+  }).done(function (response) {
+    console.log(response);
+  });
+};
+
+//Search the albums of an artist, given the id of the artist
+Spotify.prototype.getArtistById = function (artistId) {
+  $.ajax({
+    type: "GET",
+    url: this.apiUrl + "v1/artists/" + artistId + "/albums",
+    headers: {
+      Authorization: "Bearer " + access_token,
+    },
+  }).done(function (response) {
+    console.log(response);
+  });
+};
+
+//This fragment is the first thing that is loaded, when the $(document).ready
+$(function () {
+  $.ajax({
+    type: "POST",
+    url: "https://accounts.spotify.com/api/token",
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader(
+        "Authorization",
+        "Basic " + btoa(client_id + ":" + client_secret)
+      );
+    },
+    dataType: "json",
+    data: { grant_type: "client_credentials" },
+  }).done(function (response) {
+    access_token = response.access_token;
+  });
+
+  var spotify = new Spotify();
+
+  $("#b_get_artist").on("click", function () {
+    spotify.getArtist($("#artist_name").val());
+  });
+
+  $("#results").on("click", ".artistId", function () {
+    spotify.getArtistById($(this).attr("data-id"));
+  });
+});
